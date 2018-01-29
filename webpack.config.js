a2a1192394f059fb65772e0f79bb2aaa44323bee
王增迪 <wangzengdi@meituan.com>
2018-01-29 21:49:31 +0800

    first plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,23 @@
 const path = require('path');
 
+function HelloWorldPlugin(options) {
+  // Setup the plugin instance with options...
+}
+
+HelloWorldPlugin.prototype.apply = function(compiler) {
+  compiler.plugin('done', function() {
+    console.log('Hello World!');
+  });
+};
+
+module.exports = HelloWorldPlugin;
 module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  plugins: [
+    new HelloWorldPlugin({ options: true }),
+  ],
 };
